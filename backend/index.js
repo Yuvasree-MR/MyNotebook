73df8d2391be0d84c7f24dea4748048deef6418f
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-15 15:20:09 +0530

    Storing Data into the Database using Mongoose Model and installing Data Validation using express-validator

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ const app = express();
 const port = 3000
 // const port = process.env.PORT || 3000;
 
+// Middleware to send request
+app.use(express.json());
+
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
+// const { body, validationResult } = require('express-validator');
 
-router.get('/', (req, res)=>{
-  const obj = {
-    a: 'thios',
-    number: 34
-  }
-  res.json(obj)
+
+// Create a User using: POST "/api/auth/"  . Doesn't require Auth
+router.post('/', (req, res)=>{
+  console.log(req.body);
+  const user = User(req.body);
+  user.save();
+  res.send(req.body);
 })
 
 module.exports = router
