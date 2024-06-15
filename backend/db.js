6e2202fcf0a30a072942398d6395bba95710c632
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-15 12:05:23 +0530

    Connected Mongoose and Tested API calls using ThunderClient

---
+++ backend/db.js
@@ -0,0 +1,40 @@
+// connecting to DB
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/"
+
+// gpt
+async function connectToMongo() {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log("Connected to Mongo Successfully!");
+  } catch (err) {
+    console.error("Failed to connect to Mongo!!!", err);
+  }
+}
+
+// comment
+// const connectToMongo = () => {
+//   mongoose.connect(mongoURI);
+//     console.log("Connected to Mongo Successfully!");
+// }
+
+// cwh - error created
+/*
+const connectToMongo = () => {
+  mongoose.connect(mongoURI, () => {
+    console.log("Connected to Mongo Successfully!");
+  })
+}
+  */
+
+// comment
+/*
+async function connectToMongo() {
+  await mongoose.connect(mongoURI).then(() => {
+    console.log("Connected to Mongo Successfully!")
+  }).catch(err =>console.log(err));
+}
+  */
+
+
+module.exports = connectToMongo;
