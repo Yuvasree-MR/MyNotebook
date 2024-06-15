4d368f942687dea4eca5e1c31779fe1c657b60cd
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-15 12:49:12 +0530

    Created Routes & Schema for Storing Notes & Users

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,22 @@ const app = express();
 const port = 3000
 // const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// default
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.get('/api/v1/login', (req, res) => {
+//   res.send('Hello Login!');
+// });
+
+// app.get('/api/v1/signup', (req, res) => {
+//   res.send('Hello Signup!');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
