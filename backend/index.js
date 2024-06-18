af3a9a2a587574ba666c583a06b86e289e285a61
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-18 11:54:42 +0530

    Creating thunderclient collection

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 connectToMongo();
 const app = express();
-const port = 3000
+const port = 5000
 // const port = process.env.PORT || 3000;
 
 // Middleware to send request
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,25 +4,43 @@ const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-// Create a User using: POST "/api/auth/"  . Doesn't require Auth
-router.post('/', [
+// Create a User using: POST "/api/auth/createuser"  . No login required.
+router.post('/createuser', [
   body('name', 'Enter a valid Name').isLength({ min: 3}),
   body('email', 'Enter a valid Email').isEmail(),
   body('password', 'Password must be atleast 5 characters').isLength({ min: 5}),
 
-], (req, res)=>{
+], async(req, res)=>{
+  // If there are errors, return Bad request and the errors
   const errors = validationResult(req);
   if(!errors.isEmpty()){
     return res.status(400).json({ errors: errors.array() })
   }
-  User.create({
+
+  // Check whether the user with this email exits already
+
+  try{
+
+  let user = await User.findOne({email: req.body.email});
+  if(user){
+    return res.status(400).json({error: "Sorry user with this email already exists"})
+  }
+  // Create a new user
+  user = await User.create({
     name: req.body.name,
     email: req.body.email,
     password: req.body.password
-  }).then(user => res.json(user))
-  .catch(err => {console.log(err)
-    res.json({error: 'Please enter a uniqye value for email', message: err.message})
   })
+  // .then(user => res.json(user))
+  // .catch(err => {console.log(err)
+  //   res.json({error: 'Please enter a unique value for email', message: err.message})
+  // })
+  res.json(user)
+
+} catch (error) {
+  console.log(error.message);
+  res.status(500).send("Some Error occured");
+}
 })
 
 module.exports = router
