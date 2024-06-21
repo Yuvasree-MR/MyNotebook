d7ad3c63058079696f96de960ced2eb79b581e92
Yuvasree-MR <yuvasreeramanujam7@gmail.com>
2024-06-21 10:25:42 +0530

    Fetching Notes from Context

--- backend/index.js
+++ backend/index.js
@@ -27,5 +27,5 @@ app.use('/api/notes', require('./routes/notes'))
 // });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`MyNotebook Backend listening at http://localhost:${port}`);
 });
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,31 @@
-// import { useState } from "react";
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
+  const notesInitial = [
+    {
+      "_id": "6673b8ac3c074ce9d502b0f3",
+      "user": "6672687c181c17864895e29c",
+      "title": "My title",
+      "description": "Please wakeup early",
+      "tag": "Personal",
+      "date": "1718859948634",
+      "__v": 0
+    },
+    {
+      "_id": "6673b9b03c074ce9d502b0f6",
+      "user": "6672687c181c17864895e29c",
+      "title": "New Note",
+      "description": "Please See the New Note",
+      "tag": "My Note",
+      "date": "1718860208606",
+      "__v": 0
+    }
+  ];
+  const [notes, setNotes] = useState(notesInitial)
 
   return(
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{notes, setNotes}}>
       {props.children}
     </NoteContext.Provider>
   )
